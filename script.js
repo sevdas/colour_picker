d07a0c1313ce73b941a19104f69c5c6acdb60875
sevdas <sshukrieva@gmail.com>
2022-02-27 12:22:55 +0000

    add color to the favourites box on user click

--- script.js
+++ script.js
@@ -22,5 +22,13 @@ function addBox(color) {
 }
 
 
+// Handle user's event interaction on click
+$(document).on('click', '#add-to-favorite', function () {
+  const colorInputValue = $('input').val() /* retrieve value from the color field */
+
+  $('input').val('') /* clear value */
+
+  addBox(colorInputValue) /* add a box with the input color */
+})
 
 
